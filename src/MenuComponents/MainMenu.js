190bbe305881c664daef9e93bb6835a0eb4de96d
Helen Liutongco <liutongcoh@gmail.com>
2018-11-16 14:45:29 -0500

    Add menu image

--- src/MenuComponents/MainMenu.js
+++ src/MenuComponents/MainMenu.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {TOGGLE_LOAD, TOGGLE_MENU} from '../actions/index';
+import candle from "../images/candle.gif";
 
 class MainMenu extends Component {
   handleClick = (event) => {
@@ -24,6 +25,7 @@ class MainMenu extends Component {
         <h1>Three Kings</h1>
         <button id="new-game" className="main-menu-buttons">New Game</button>
         <button id="load-game" className="main-menu-buttons">Load Game</button>
+        <div id="candle"><img src={candle}/></div>
       </div>
     )
   }
